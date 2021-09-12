5bd9db588ee6f16aa7b98280e3e76f0efb58a789
CapitaineCourage <ndjomeni.com@gmail.com>
2021-09-12 17:58:09 +0200

    save changed

---
+++ bigdata-dev/arnaud/appli/diamond/shell/ddl_diamod_sio.sh
@@ -0,0 +1,10 @@
+# to do
+# afficher le nom du script en cours d'éexécution
+# récupérer la date de début d'exécution 
+
+
+mysql -h <hostname> -u <username> --password=<password> -D <database> -e 'source <path-to-sql-file>'
+
+# récuperer la date de fin d'exécution du sql
+# afficher la duré du traitement
+# rediriger les logs dans un fichier log le fichier vas porter le nom du sh_timestamp.log (à stoquer dans logapp)
---
+++ bigdata-dev/arnaud/appli/diamond/shell/ingestion_diamond_sio.sh
@@ -0,0 +1,11 @@
+# to do
+# breve recherche sur l'outils sqoop
+# consulter le nomnre de lignes de la table customer à l'aide de sqoop (sqoop eval)
+# consulter le nombre de commande (de la table ) ayant un prix supérieur à 20. (sqoop eval)
+# importer toutes les tables sur HDFS (sqoop import)
+# inporter toutes les tables avec le format ORC (sqoop import)
+# importer toutes les tables dans une table cible Hive (sqoop import)
+# copier les fichiers dataset sur HDFS (commande HDFS)
+# cosulter la taille des fichiers copiés sur HDFS (commande HDFS)
+# Afficher le contenu d'un des fichiers HDFS copié (commande HDFS)
+# faire une copie d'un fichier datset de HDFS vers le système de fichier local (commande HDFS)
